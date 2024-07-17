e9871094b80cf2863042ccce8a91d518b8625c8a
EC2 Default User <ec2-user@ip-172-31-46-191.ap-northeast-1.compute.internal>
2024-07-17 11:54:56 +0000

    メンターさんとの修正後のbookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,19 +4,28 @@ class BooksController < ApplicationController
   end
 
   def create
-  book = Book.new(book_params)
-  book.save
-  redirect_to book_path(book.id)
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
   
   private
